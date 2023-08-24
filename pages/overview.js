34c764aab207939a60785a1f23b6dbdd24f980ea
zekraquraan <zekraquraan7@gmail.com>
2023-08-24 21:23:15 +0000

    12

--- pages/overview.js
+++ pages/overview.js
@@ -1,4 +1,4 @@
-import Hhead from '../components/Hhead';
+import Hhead from '../components/Head';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Login from './login';
